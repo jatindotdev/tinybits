8e4bf00d87715185a8c01b57ad4523d9bfe9d205
Jatin Kumar <main.jatink@gmail.com>
2024-01-19 23:59:22 +0530

    feat: input field with validation

--- client/app/routes/_index.tsx
+++ client/app/routes/_index.tsx
@@ -1,6 +1,6 @@
-import { GitHubLogoIcon } from '@radix-ui/react-icons';
+import { GitHubLogoIcon, Link1Icon } from '@radix-ui/react-icons';
 import { json, type LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { Form, useLoaderData } from '@remix-run/react';
 import { Nav } from '~/components/nav';
 import { Button } from '~/components/ui/button';
 import { Section } from '~/components/ui/section';
@@ -19,7 +19,7 @@ export default function Index() {
     <>
       <Nav user={user} />
       <Section className="container items-center text-center pt-14">
-        <div className="mx-auto mb-10 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0 text-pretty">
+        <div className="mx-auto mb-6 mt-12 max-w-md px-2.5 text-center sm:max-w-lg sm:px-0 text-pretty">
           <h1 className="scroll-m-20 text-3xl font-bold tracking-tight first:mt-0 lg:text-4xl">
             Our URL shortener makes link sharing and tracking a breeze.
           </h1>
@@ -38,6 +38,42 @@ export default function Index() {
               <GitHubLogoIcon /> GitHub
             </a>
           </Button>
+        </div>
+        <div className="mx-auto w-full max-w-md px-2.5 sm:px-0">
+          <Form method="post" action="/auth/github">
+            <div className="relative flex items-center">
+              <Link1Icon className="size-5 text-gray-400 absolute inset-x-0 ml-2.5" />
+              <input
+                type="url"
+                placeholder="https://github.com/jatindotdev"
+                autoComplete="off"
+                required
+                className="peer block w-full rounded-md border border-gray-200 bg-white p-2 pl-10 pr-12 shadow-lg focus:border-black focus:outline-none focus:ring-0 sm:text-sm"
+                name="url"
+              />
+              <button
+                type="submit"
+                className="hover:border-gray-700 hover:text-gray-700 peer-focus:text-gray-700 absolute inset-y-0 right-0 my-1.5 mr-1.5 flex w-10 items-center justify-center rounded border border-gray-200 font-sans text-sm font-medium text-gray-400"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  className="size-4"
+                >
+                  <title>Shorten</title>
+                  <polyline points="9 10 4 15 9 20" />
+                  <path d="M20 4v7a4 4 0 0 1-4 4H4" />
+                </svg>
+              </button>
+            </div>
+          </Form>
         </div>
       </Section>
     </>
